10f9da07377842a6e8556eef152c5012c1b934c9
David <email": null>
2016-06-25 20:41:38 -0700

    got register form to work and lnow have to work on bycrypt

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,3 +1,22 @@
+get 'users' do
+  @users = User.all
+  erb :'users/index'
+end
+
+
 get '/users/new' do
     erb :'users/new'
+end
+
+post '/users' do
+  @user = User.create(first_name: params[:first_name], last_name: params[:last_name], username: params[:username], email: params[:email],password: params[:password] )
+
+    session[:id] = @user.id
+    redirect "/users/#{@user.id}"
+
+end
+
+get '/users/:id' do
+  @usser = find(params[:id])
+  erb :'users/show'
 end
